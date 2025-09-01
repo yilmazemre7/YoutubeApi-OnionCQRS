b26824c409390592486bc2ac5a2de97aefcd8e38
Emrecan Yılmaz <emrecan.yilmaz@penta.com.tr>
2025-09-01 11:04:40 +0300

    projenin development ve production alanları ayrıştırıldı.

--- YoutubeApi.Api/Program.cs
+++ YoutubeApi.Api/Program.cs
@@ -6,6 +6,13 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
