827f7d36849988c784856363ec7112746ab00cb4
Emrecan Yılmaz <emrecan.yilmaz@penta.com.tr>
2025-09-01 12:14:52 +0300

    temel entityler olusturuldu.

---
+++ Core/YoutubeApi.Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/YoutubeApi.Domain/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+
+        }
+        public Category(int parentId, string name, int priority)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priority = priority;
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products{ get; set; }
+    }
+}
