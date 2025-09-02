e8595f5163c81dd4af1331055673323dbf7bc9a7
Emrecan Yılmaz <emrecan.yilmaz@penta.com.tr>
2025-09-02 13:49:14 +0300

    unitofwork eklendi

--- Core/YoutubeApi.Domain/Entities/Category.cs
+++ Core/YoutubeApi.Domain/Entities/Category.cs
@@ -15,9 +15,9 @@ namespace YoutubeApi.Domain.Entities
             Name = name;
             Priority = priority;
         }
-        public required int ParentId { get; set; }
-        public required string Name { get; set; }
-        public required int Priority { get; set; }
+        public  int ParentId { get; set; }
+        public  string Name { get; set; }
+        public  int Priority { get; set; }
         public ICollection<Detail> Details { get; set; }
         public ICollection<Product> Products{ get; set; }
     }
